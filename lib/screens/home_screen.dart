aa00929ee3e2d8f140df6a96ca626290ed6de81b
Lucas Moyle <57916028+lmoyle1989@users.noreply.github.com>
2022-02-21 18:45:32 -0800

    found a workaround i guess

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_journal/components/entry_list_view.dart';
 import 'package:my_journal/components/journal_scaffold.dart';
+import 'package:my_journal/screens/entry_detail_screen.dart';
 import '../components/master_detail_view.dart';
 import 'new_entry_screen.dart';
 
@@ -14,18 +15,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Widget myBody = EntryListView(
+    key: UniqueKey(),
+  );
+
   @override
   Widget build(BuildContext context) {
     return JournalScaffold(
       title: "List of Journal Entries",
       actionButton: newEntryButton(context),
-      body: EntryListView(),
+      body: myBody,
       horizontalBody: const MasterDetailView(),
     );
   }
 
+  void resetListView() {
+    setState(() {
+      myBody = EntryListView(
+        key: UniqueKey(),
+      );
+    });
+  }
+
   void pushNewEntryScreen(BuildContext context) {
-    Navigator.of(context).pushNamed(NewEntryScreen.routeName);
+    Navigator.of(context)
+        .pushNamed(NewEntryScreen.routeName)
+        .then((value) => resetListView());
   }
 
   FloatingActionButton newEntryButton(BuildContext context) {
